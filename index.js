955f50a3136226529c50df0829f267042b6665aa
Zensynthium <47935832+Zensynthium@users.noreply.github.com>
2022-02-08 13:43:27 -0500

    Update index.js

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ app.post('/verify-token', cors(corsOptions), (req, res) => {
   	})
 
   	const payload = ticket.getPayload()
-  	const userid = payload['sub']
+  	// const userid = payload['sub']
 
   	// If request specified a G Suite domain:
   	// const domain = payload['hd']
